0adb196e42884bcd7f0d575dcce30f05ff0ac6f3
KeevinF <123892030+KeevinF@users.noreply.github.com>
2023-02-09 11:47:55 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -8,9 +8,11 @@ function colocarNaTela (dados){
 
     document.querySelector(".cidade").innerHTML ="Tempo em " + dados.name
     document.querySelector(".temp").innerHTML = Math.floor (dados.main.temp) + "°C"
+    document.querySelector(".temp-max").innerHTML = "Max " + Math.floor(dados.main.temp_max) + "°C"
+    document.querySelector(".temp-min").innerHTML =  "Min " + Math.floor(dados.main.temp_min) + "°C"
     document.querySelector(".descricao").innerHTML= dados.weather [0].description
     document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
-    document.querySelector(".umidade").innerHTML = "Umidade " + dados.main.humidity
+    document.querySelector(".umidade").innerHTML = "Umidade " + dados.main.humidity + "%"
 
 }
 
